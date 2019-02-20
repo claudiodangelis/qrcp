c877226eeba9a113f821972f55411804f75ee534
Claudio d'Angelis <claudiodangelis@gmail.com>
2019-02-20 17:54:00 +0100

    minor code cleanup

--- tcpKeepAliveListener.go
+++ tcpKeepAliveListener.go
@@ -1,3 +1,5 @@
+package main
+
 // Copyright (c) 2009 The Go Authors. All rights reserved.
 
 // Redistribution and use in source and binary forms, with or without
@@ -25,8 +27,6 @@
 // THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-package main
 
 import (
 	"net"
