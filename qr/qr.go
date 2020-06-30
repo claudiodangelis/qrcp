d139d6f1cb6ee403f680f5feeef4d2860fd91901
Anoop B <16367697+anoop-b@users.noreply.github.com>
2020-06-30 19:53:37 +0530

    write png to temp file

--- qr/qr.go
+++ qr/qr.go
@@ -2,7 +2,12 @@ package qr
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"runtime"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/skip2/go-qrcode"
 )
 
@@ -10,7 +15,31 @@ import (
 func RenderString(s string) {
 	q, err := qrcode.New(s, qrcode.Medium)
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
+
+	if runtime.GOOS == "windows" {
+		//create png from string
+		png, err := qrcode.Encode(s, qrcode.Medium, 256)
+		if err != nil {
+			log.Panic(err)
+		}
+		//create temp file in temp dir
+		f, err := ioutil.TempFile(os.TempDir(), "qrcp*.png")
+		if err != nil {
+			log.Panic(err)
+		}
+		//remove temp file after use
+		defer os.Remove(f.Name())
+
+		//write png to temp file
+		_, err = f.Write(png)
+		if err != nil {
+			log.Panic(err)
+		}
+		//open temp file using native file-handler
+		exec.Command("rundll32", "url.dll,FileProtocolHandler", f.Name()).Start()
+	}
+
 	fmt.Println(q.ToSmallString(false))
 }
