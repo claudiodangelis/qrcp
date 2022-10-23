77ffc236d6d3f3ae62897e48cb6f380379abcd61
Claudio d'Angelis <claudiodangelis@gmail.com>
2022-10-23 16:17:29 +0200

    complete migration

--- config/migrate.go
+++ config/migrate.go
@@ -1,13 +1,14 @@
 package config
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/adrg/xdg"
+	"gopkg.in/yaml.v2"
 )
 
 // migrate function will look for an existing legacy configuration file
@@ -15,15 +16,13 @@ import (
 func migrate() {
 	// Return if old file does not exist
 	oldConfigFile := filepath.Join(xdg.ConfigHome, "qrcp", "config.json")
-	newConfigFile := filepath.Join(xdg.ConfigHome, "qrcp", "config.json")
+	newConfigFile := filepath.Join(xdg.ConfigHome, "qrcp", "config.yml")
 
 	if _, err := os.Stat(oldConfigFile); os.IsNotExist(err) {
-		fmt.Println("old config file does not exist, skipping")
 		return
 	}
 	// Delete old file if new file exists and return
 	if _, err := os.Stat(newConfigFile); !os.IsNotExist(err) {
-		fmt.Println("new config file exists, delete old")
 		if err := os.Remove(oldConfigFile); err != nil {
 			fmt.Println("warning: error while deleting old configuration file", err)
 		}
@@ -35,10 +34,32 @@ func migrate() {
 		fmt.Println("warning: error while reading contents of old configuration file", err)
 		return
 	}
-	var oldConfigStruct interface{}
-	if err := json.Unmarshal(oldConfig, &oldConfigStruct); err != nil {
+	var oldConfigStruct struct {
+		Fqdn        string `yaml:"fqdn,omitempty"`
+		Iface       string `yaml:"interface,omitempty"`
+		Keepalive   bool   `yaml:"keepalive,omitempty"`
+		Output      string `yaml:"output,omitempty"`
+		Path        string `yaml:"path,omitempty"`
+		Port        int    `yaml:"port,omitempty"`
+		Questaprova string `yaml:"questaprova,omitempty"`
+		Secure      bool   `yaml:"secure,omitempty"`
+		Tlscert     string `yaml:"tls-cert,omitempty"`
+		Tlskey      string `yaml:"tls-key,omitempty"`
+	}
+	// Heads-up: Replace tls-cert, tls-key with tlscert, tlskey
+	if err := yaml.Unmarshal(oldConfig, &oldConfigStruct); err != nil {
 		fmt.Println("warning: error while parsing JSON from old configuration file", err)
 		return
 	}
-	fmt.Printf("%+v\n", oldConfigStruct)
+	newConfig, err := yaml.Marshal(oldConfigStruct)
+	if err != nil {
+		fmt.Println("warning: error while migrating JSON file to YAML file", err)
+	}
+	if err := os.WriteFile(newConfigFile, []byte(strings.ReplaceAll(string(newConfig), "tls-", "tls")), 0644); err != nil {
+		fmt.Println("warning: error while creating the migrated YAML file", err)
+	}
+	// Delete old file
+	if err := os.Remove(oldConfigFile); err != nil {
+		fmt.Println("warning: error while deleting old configuration file", err)
+	}
 }
